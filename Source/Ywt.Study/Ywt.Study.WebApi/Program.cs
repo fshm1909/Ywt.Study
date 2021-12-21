b61d2f1eb17a428f7f7f703121141419b51ec0ef
ywt <1392733496@qq.com>
2021-12-21 21:23:44 +0800

    fix:Startup和Program的一些注释

--- Source/Ywt.Study/Ywt.Study.WebApi/Program.cs
+++ Source/Ywt.Study/Ywt.Study.WebApi/Program.cs
@@ -13,14 +13,29 @@ namespace Ywt.Study.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //IHostBuilder和IHost；IHostBuilder用于配制，IHost用于运行
+
+            CreateHostBuilder(args)
+                .Build()//运行给定的操作以初始化主机。这只能调用一次。
+                        //.Start()//同步启动主机。
+                .Run();//运行应用并阻止调用线程，直到关闭主机。
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        /// <summary>
+        /// 生成器
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                 .CreateDefaultBuilder(args)//使用预先配置的默认值初始化Microsoft.Extensions.Hosting.HostBuilder类的新实例。
+                                            
+                 //使用默认值配置Microsoft.Extensions.Hosting.IHostBuilder以托管web应用。
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseStartup<Startup>();
+                 });
+        }
     }
 }
--- Source/Ywt.Study/Ywt.Study.WebApi/Startup.cs
+++ Source/Ywt.Study/Ywt.Study.WebApi/Startup.cs
@@ -16,17 +16,30 @@ namespace Ywt.Study.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        public IHostApplicationLifetime AppLifetime { get; }
+
+        /// <summary>
+        /// 构造函数依赖注入
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="environment"></param>
+        /// <param name="appLifetime"></param>
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment, IHostApplicationLifetime appLifetime)
         {
             Configuration = configuration;
+            Environment = environment;
+            AppLifetime = appLifetime;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// （可选）
+        /// 配置应用的服务（注册服务，并通过依赖关系注入 (DI) 或 ApplicationServices 在整个应用中使用服务。）
+        /// </summary>
+        /// <param name="services">服务容器</param>
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,9 +47,25 @@ namespace Ywt.Study.WebApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /// <summary>
+        /// （必选）
+        /// 配制管道（中间件）
+        /// </summary>
+        /// <param name="app">应用程序生成器（可通过将中间件组件添加到 IApplicationBuilder 实例来配置请求管道）</param>
+        /// <param name="env">主机环境设置</param>
+        /// <param name="appLifetime">主机应用程序生存期</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime appLifetime)
         {
+            //通用环境设置
+            string ApplicationName = env.ApplicationName;//包含应用入口点的程序集的名称。
+            string ContentRoot = env.ContentRootPath;//应用程序集所在的文件夹。
+            string EnvironmentName = env.EnvironmentName;//环境名称（框架定义的值包括 Development、Staging 和 Production）
+
+            //IHostApplicationLifetime(主机应用程序生存期) 允许后启动和关闭活动。 接口上的三个属性是用于注册 Action 方法（用于定义启动和关闭事件）的取消标记。
+            //appLifetime.ApplicationStarted.Register(() => { });
+            //appLifetime.ApplicationStopping.Register(() => { });
+            //appLifetime.ApplicationStopped.Register(() => { });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
